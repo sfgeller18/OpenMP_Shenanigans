e0d94f1d46a622069c3330615622a7bc2501aa96
sfgeller18 <122637296+sfgeller18@users.noreply.github.com>
2023-06-13 10:32:51 -0700

    Create OMP_MC.cpp

---
+++ OMP_MC.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <omp.h>
+#include <random>
+#include <cmath>
+
+#define PI 3.141592653589793
+
+//Generates n samples (Please enter n a multiple of thread_count :) )
+void Box_Muller(int n, char* method) {
+    int m = omp_get_max_threads();
+	int num_iter{ 0 };
+	double* nums;
+	if ("antithetic" == method) { int num_iter = n / (2 * m); }
+	else { int num_iter = n / (2 * m); }
+	std::mt19937 rng;
+	rng.seed(1234);
+	std::uniform_real_distribution<double> gen(0.0, 1.0);
+#pragma omp parallel 
+	nums = (double*) calloc(num_iter, sizeof(double));
+	#pragma omp for
+	for (int i = 0; i < num_iter; i++) {
+		double u1{ 0.0 };
+		double u2{ 0.0 };
+		u1 = gen(rng);
+		u2 = gen(rng);
+		double r = sqrt(-2.0f * log(u1));
+		nums[i] = r * cos(2.0 * PI * u2);
+		nums[num_iter + i] = r * sin(2.0 * PI * u2);
+		if ("antithetic" == method) { nums[2 * num_iter + i] = -r * cos(2.0f * PI * u2); nums[3 * num_iter + i] = -r * sin(2.0f * PI * u2); }
+	}
+}
+
+int main()
+{
+	int maxThreads = omp_get_max_threads();
+	std::cout << "Maximum number of threads: " << maxThreads << std::endl;
+	return 0;
+}
